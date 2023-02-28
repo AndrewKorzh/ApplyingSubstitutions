3a49951488ca0d79cffb88c9444e1976558be8d4
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-03-01 02:58:20 +0300

    Ok?

---
+++ ApplyingSubstitutionsProg/ApplyingSubstitutionsProg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplyingSubstitutionsProg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JustTest!");
+        }
+    }
+}
