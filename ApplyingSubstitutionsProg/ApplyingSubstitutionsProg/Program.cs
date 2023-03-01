70de48b8107ddf03b8c15a8257bed76dd95379c5
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-03-01 03:05:38 +0300

    Create class Sub
    Создание класса ПОДСТАНОВКА

--- ApplyingSubstitutionsProg/ApplyingSubstitutionsProg/Program.cs
+++ ApplyingSubstitutionsProg/ApplyingSubstitutionsProg/Program.cs
@@ -7,4 +7,29 @@
             Console.WriteLine("JustTest!");
         }
     }
+    class SUBSTITUTION
+    {
+
+        public string from;
+        public string to;
+        public bool the_final;
+        public SUBSTITUTION(string f, string t, bool t_f = false)
+        {
+            from = f;
+            to = t;
+            the_final = t_f;
+        }
+
+        public string ToStr()
+        {
+            return $"{(from == "" ? "_" : from)} ->{(the_final ? ". " : " ")}{(to == "" ? "_" : to)}";
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToStr());
+        }
+
+
+
+    }
 }
