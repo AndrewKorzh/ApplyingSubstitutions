159417de83c3b79b10122915c2212a0abac19ac8
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-03-01 03:06:50 +0300

    Class Word

--- ApplyingSubstitutionsProg/ApplyingSubstitutionsProg/Program.cs
+++ ApplyingSubstitutionsProg/ApplyingSubstitutionsProg/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ApplyingSubstitutionsProg
+﻿using System.Text;
+
+namespace ApplyingSubstitutionsProg
 {
     internal class Program
     {
@@ -7,7 +9,97 @@
             Console.WriteLine("JustTest!");
         }
     }
-    class SUBSTITUTION
+    class WORD
+    {
+        public StringBuilder word = new StringBuilder("");
+        public List<SUBSTITUTION> substitutions;
+        public List<string> history_of_words;
+
+        public WORD(string w, List<SUBSTITUTION> s)
+        {
+            word = new StringBuilder(w); this.substitutions = s; history_of_words = new List<string>();
+        }
+
+        /// <summary>
+        /// Попытка применения подстановки
+        /// </summary>
+        /// <param name="sub"></param>
+        /// <returns></returns>
+        public bool attempt_to_apply_substitution(SUBSTITUTION sub)
+        {
+            if (word.ToString().Contains(sub.from))
+            {
+                var i = word.ToString().IndexOf(sub.from);
+
+                word.Remove(i, sub.from.Length);
+                word.Insert(i, sub.to);
+                if (sub.the_final)
+                {
+                    return false;
+                }
+                return true;
+            }
+            else return false;
+        }
+        /// <summary>
+        /// Попытка применения подстановок из списка
+        /// </summary>
+        /// <returns></returns>
+        public bool attempt_to_apply_substitution_from_list()
+        {
+            for (int i = 0; i < substitutions.Count; i++)
+            {
+
+                if (word.ToString().Contains(substitutions[i].from))
+                {
+                    Print();
+                    if (attempt_to_apply_substitution(substitutions[i]))
+                        return true;
+                    return false;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Применение подстановок
+        /// </summary>
+        public void applying_substitutions()
+        {
+            while (true)
+            {
+
+                if (!attempt_to_apply_substitution_from_list())
+                {
+                    Print();
+                    return;
+                }
+                //Замедление - чтобы в случае бесконечного увеличенияя слова - было понятно, что происходит
+                System.Threading.Thread.Sleep(500);
+                if (history_of_words.Contains(word.ToString()))
+                {
+
+                    Console.WriteLine($"слово {word.ToString()} уже встречалось");
+                    return;
+                }
+                history_of_words.Add(word.ToString());
+            }
+
+        }
+        public string ToStr()
+        {
+            if (word.ToString() == "")
+                return "_";
+            return word.ToString();
+        }
+        public void Print()
+        {
+
+            Console.WriteLine(ToStr());
+        }
+
+
+
+        class SUBSTITUTION
     {
 
         public string from;
